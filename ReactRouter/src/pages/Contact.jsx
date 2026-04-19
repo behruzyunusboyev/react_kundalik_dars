0198364a08ea19acf0ec2546a453c63174683e54
Behruz <behruzyunusboyev68@gmail.com>
2026-04-19 22:06:51 +0500

    aboutga iconlar kerak

--- ReactRouter/src/pages/Contact.jsx
+++ ReactRouter/src/pages/Contact.jsx
@@ -2,24 +2,31 @@ import github_icon from "../pages/github_icon.jpg"
 import email_icon from "../pages/email_icon.jpg"
 import tg_icon from "../pages/tg_icon.jpg"
 import "../pages/styles/contact.css"
+import { useRef } from "react"
 function Cuntact(){
+    const refs = useRef([])
+
+    const nushalash = (index) => {
+        const text= refs.current[index].innerText
+        navigator.clipboard.writeText(text)
+    }
     return(
         <div className="cuntact_div" style={{marginTop:'70px'}}>
-            <h1>Men bilan aloqa</h1>
+            <h1 id="main_h1">Men bilan aloqa</h1>
             <div className="yollar">
                 <img src={github_icon} alt="github_icon" />
                 <h1>Git Hub:</h1>
-                <p>behruzyunusboyev</p>
+                <p ref={(el) => (refs.current[0] = el) } onClick={() => nushalash(0)}>behruzyunusboyev</p>
             </div>
             <div className="yollar">
                 <img src={email_icon} alt="email_icon" />
                 <h1>Email:</h1>
-                <p id="emailp">behruzyunusboyev9@gmail.com</p>
+                <p id="emailp" ref={(el) => (refs.current[1] = el) } onClick={() => nushalash(1)}>behruzyunusboyev9@gmail.com</p>
             </div>
             <div className="yollar">
                 <img src= {tg_icon} alt="" />
                 <h1>Telegram:</h1>
-                <p>@Be1ruz_1310</p>
+                <p ref={(el) => (refs.current[2] = el) } onClick={() => nushalash(2)}>t.me/Be1ruz_1310</p>
             </div>
         </div>
     )
--- ReactRouter/src/pages/about.jsx
+++ ReactRouter/src/pages/about.jsx
@@ -3,13 +3,13 @@ function About(){
     return(
         <div id='about_main'>
             <h1>Men Behruz Yunusboyev</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis excepturi sequi numquam maxime, libero dolore consequatur ab fugiat 
-                reprehenderit ut suscipit nulla modi perferendis molestiae odio id beatae at, soluta laborum veniam, explicabo inventore.
-                 Fugit repudiandae laborum deserunt ipsum eveniet sequi veritatis incidunt, sunt reprehenderit aliquam voluptatem a unde vitae. </p>
+            <p>Hozirda 16 yoshdaman, O'zekiston Xorazmdagi Alxorazmiy loyhasida oqiman. 
+            1.5 yilldan beri frontend devloperlik sohasini organib kelaman.Shu kungacha bir nechta
+             kichik loyhalarda ishlagan man. Men blan ishlashni hohlasangiz contact bolimida bog'laning </p>
+             <div className="icons">
+                
+             </div>
         </div>
     )
 }
 export default About
-
-// Hozirda 16 yoshdaman, O'zekiston Xorazmdagi Alxorazmiy loyhasida oqiman. 1.5 yilldan beri frontend devloperlik sohasini organib kelaman.
-                // Shu kungacha bir nechta kichik loyhalarda ishlagan man. Men blan ishlashni hohlasangiz contact bolimida bog'laning
